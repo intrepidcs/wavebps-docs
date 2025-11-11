34b187b1469dc223be3cb1a9186b838a0ed5e713
Praavin Bawwane <pbawane@freewili.com>
2025-11-11 11:00:40 +0530

    youtube embed video error fix package updated

--- src/components/YouTubeEmbed.js
+++ src/components/YouTubeEmbed.js
@@ -1,15 +1,53 @@
+// import React from 'react';
+
+// export default function YouTubeEmbed({ videoId, caption }) {
+//   return (
+//     <div style={{ maxWidth: '100%', marginBottom: '2rem' }}>
+//       <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
+//         <iframe
+//           src={`https://www.youtube.com/embed/${videoId}`}
+//           frameBorder="0"
+//           allow="autoplay; encrypted-media"
+//           allowFullScreen
+//           title="YouTube video"
+//           style={{
+//             position: 'absolute',
+//             top: 0,
+//             left: 0,
+//             width: '100%',
+//             height: '100%',
+//           }}
+//         />
+//       </div>
+//       {caption && (
+//         <div style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#666' }}>
+//           {caption}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+
 import React from 'react';
 
 export default function YouTubeEmbed({ videoId, caption }) {
+  // Detect current domain (used for the origin param)
+  const origin =
+    typeof window !== 'undefined'
+      ? window.location.origin
+      : 'https://guide.intrepidcs.com';
+
   return (
     <div style={{ maxWidth: '100%', marginBottom: '2rem' }}>
       <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
         <iframe
-          src={`https://www.youtube.com/embed/${videoId}`}
+          src={`https://www.youtube.com/embed/${videoId}?origin=${origin}`}
           frameBorder="0"
-          allow="autoplay; encrypted-media"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           title="YouTube video"
+          referrerPolicy="strict-origin-when-cross-origin"
           style={{
             position: 'absolute',
             top: 0,
@@ -20,7 +58,14 @@ export default function YouTubeEmbed({ videoId, caption }) {
         />
       </div>
       {caption && (
-        <div style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#666' }}>
+        <div
+          style={{
+            textAlign: 'center',
+            marginTop: '0.5rem',
+            fontStyle: 'italic',
+            color: '#666',
+          }}
+        >
           {caption}
         </div>
       )}
